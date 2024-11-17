a412a76041d974e7a2f9b48e0ec95ee7a88c2d2e
Jonas Hoffmann <jonashoffmanns@gmail.com>
2024-11-17 16:33:59 +0100

    fiks

--- graph3.sh
+++ graph3.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#BSUB -J GraphBuilder
+#BSUB -J GraphProcess
 #BSUB -q hpc
-#BSUB -R "rusage[mem=32GB]"
-#BSUB -R "select[model == XeonGold6226R]"
+#BSUB -R "rusage[mem=64GB]"
+#BSUB -R "select[model == EPYC9554]"
 #BSUB -B
 #BSUB -N
 ##BSUB -u s204071@dtu.dk
 #BSUB -o Output_%J.out
 #BSUB -e Output_%J.err
-#BSUB -W 01:00 
-#BSUB -n 16
+#BSUB -W 24:00 
+#BSUB -n 4
 #BSUB -R "span[hosts=1]"
 
 source .venv/bin/activate
 
-python graph2.py
+python graph3_process.py
